d7b2c22c0e30cfbb2d2bb15229daf4effd9843b7
wei.zhou <wei.zhou@shunwang.om>
2017-12-01 09:34:43 +0800

    u

--- main.go
+++ main.go
@@ -2,95 +2,46 @@ package main
 
 import (
 	"golog/log"
-	"flag"
-	"time"
-	"sync"
-	"sync/atomic"
+	"fmt"
+	"net"
 	"os"
-	"os/signal"
-	"runtime/pprof"
-	"strings"
 )
 
-func udpMessage(snbid string, key string, data interface{})  {
+var host = "log.wxdesk.com"
+var port = "22002"
 
-	header_bytes := "{\"key\": " + key +"}"  //ujson.dumps({"key": key}, ensure_ascii=False).encode()
-	header_length := strings. header_bytes)
-	//body_length = len(body_bytes)
-	//total_length = header_length + body_length + 12
-	//data = b'sicent%06x%06x%s%06x%s' % (total_length, header_length, header_bytes, body_length, body_bytes)
-	//udp_socket.sendto(data, (GOLOG_ADDRESS[0], GOLOG_ADDRESS[1]))
-	//
-
+func udpSend(s string) {
+	addr, err := net.ResolveUDPAddr("udp", host+":"+port)
+	if err != nil {
+		fmt.Println("Can't resolve address: ", err)
+		os.Exit(1)
+	}
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn.Close()
+	conn.Write([]byte(s))
+	fmt.Printf("<%s>\n", conn.RemoteAddr())
 }
+func udpMessage(snbid string, key string, body_bytes string) {
+	header_bytes := "{\"key\": \"" + key + "\"}" //ujson.dumps({"key": key}, ensure_ascii=False).encode()
+	header_length := len(header_bytes)
+	body_length := len(body_bytes)
+	total_length := header_length + body_length + 12
+	s := fmt.Sprintf("sicent%06x%06x%s%06x%s", total_length, header_length, header_bytes, body_length, body_bytes)
+	logger.LInfo(s)
+
+	udpSend(s)
+}
+
+var logger = log.GetLogger("./logs/app")
 
 func main() {
-
-	cpuprofile := "perf.prof"
-	if cpuprofile != "" {
-		f, err := os.Create(cpuprofile)
-		if err != nil {
-			//logger.LFatal("%s", err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-	}
-	logger1 := log.GetLogger("./logs/xyz")
-	defer log.Uninit(logger1)
-
-	logger := log.GetLogger("./logs/app")
 	defer log.Uninit(logger)
-
+	for ii := 0; ii < 100; ii++ {
+		udpMessage("huqiuxia", "testudplog", "{\"snbid\":\"huqiuxia\",\"test\":\"ok\"}")
+	}
 	log.SetLevel(log.LvTRACE)
-
-	flag.Parse()
-	d := time.Duration(0) * time.Millisecond
-
-	running := int32(1)
-
-	var g sync.WaitGroup
-
-	task := func() {
-		for atomic.LoadInt32(&running) != 0 {
-			logger.LTrace("hello %s", "Trace")
-			logger.LDebug("hello %s", "Debug")
-			logger.LInfo("hello %s", "Info")
-			logger.LWarn("hello %s", "Warn")
-			logger.LError("hello %s", "Error")
-
-			logger1.LTrace("hello %s", "Trace")
-			logger1.LDebug("hello %s", "Debug")
-			logger1.LInfo("hello %s", "Info")
-			logger1.LWarn("hello %s", "Warn")
-			logger1.LError("hello %s", "Error")
-
-			if d > 0 {
-				time.Sleep(d)
-			}
-		}
-		g.Done()
-	}
-
-	n := 10
-	g.Add(n)
-	for i := 0; i < n; i++ {
-		go task()
-	}
-
-	listenSignal(func(sig os.Signal) bool {
-		atomic.StoreInt32(&running, 0)
-		return true
-	})
-
-	g.Wait()
 	logger.LInfo("app exit")
-}
-
-func listenSignal(handler func(sig os.Signal) (ret bool), signals ...os.Signal) {
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, signals...)
-	go func() {
-		for !handler(<-sigChan) {
-		}
-	}()
 }
